05398aebac423b10cad853af3a922961e84a0c75
0xff01 <0xff01@gmail.com>
2014-11-20 13:20:54 +0100

    added and modified mode filter, not tested

--- src/de/ese/beatit/PulseControl.java
+++ src/de/ese/beatit/PulseControl.java
@@ -1,7 +1,11 @@
 package de.ese.beatit;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.math.*;
 
 public class PulseControl extends Thread
@@ -16,7 +20,9 @@ public class PulseControl extends Thread
 	public static final int HIST_SIZE = 300;
 	
 	public static final int MEAN_SIZE = 20;
+	private int meanFilterCnt = 0;
 	public static final int MODE_SIZE = 90;
+	private int modeFilterCnt = 0;
 	
 	public final int FREQ_HZ = 1;
 	
@@ -24,15 +30,20 @@ public class PulseControl extends Thread
 	@Override public void run()
 	{
 		// mean stuff
-		ArrayList<Integer> meanFilterList = new ArrayList<Integer>();
-		meanFilterList.ensureCapacity(MEAN_SIZE);
+
+		ArrayDeque<Integer> meanFilterList = new ArrayDeque<Integer>();
+		//ArrayList<Integer> meanFilterList = new ArrayList<Integer>();
+		//meanFilterList.ensureCapacity(MEAN_SIZE);
 		int sum = 0;
 		int mean = 0;
-		int mode = 0;
+		int modeKey = 0;
+		int modeVal = 0;
 		
 		// mode stuff
-		ArrayList<Integer> modeFilterList = new ArrayList<Integer>();
-		modeFilterList.ensureCapacity(MODE_SIZE);
+		//ArrayList<Integer> modeFilterList = new ArrayList<Integer>();
+		//modeFilterList.ensureCapacity(MODE_SIZE);
+		ArrayDeque<Integer> modeFilterList = new ArrayDeque<Integer>();
+		Map<Integer, Integer> modeMap = new HashMap<Integer, Integer>();
 		
 		while(true)
 		{
@@ -71,16 +82,75 @@ public class PulseControl extends Thread
 			}
 			
 			// use mean filter
-			meanFilterList = (ArrayList<Integer>) actPulseHist.subList(HIST_SIZE-1-MEAN_SIZE, HIST_SIZE-1);
+			//meanFilterList = (ArrayList<Integer>) actPulseHist.subList(HIST_SIZE-1-MEAN_SIZE, HIST_SIZE-1);
+			/*
 			for(int i = 0; i <= MEAN_SIZE; i++)
 			{
 				sum += meanFilterList.get(i);
 			}
+			*/
+			meanFilterList.addFirst(mean);
+			if (meanFilterCnt > MEAN_SIZE)
+			{
+				sum += actPulse;
+				sum -= meanFilterList.removeLast();
+			}
+			else
+			{
+				sum+= actPulse;
+				meanFilterCnt++;
+			}
 			mean = sum/MEAN_SIZE;
 			
 			// use mode filter
-			Collections.rotate(modeFilterList, -1);
-			modeFilterList.set(MODE_SIZE-1, mean);
+			//Collections.rotate(modeFilterList, -1);
+			//modeFilterList.set(MODE_SIZE-1, mean);
+			//modeFilterList.
+			modeFilterList.addFirst(mean);
+			if (modeMap.containsKey(mean))
+			{
+				modeMap.put(mean, modeMap.get(mean) + 1);
+			}
+			else
+			{
+				modeMap.put(mean, 1);
+			}
+			if (modeFilterCnt > MODE_SIZE)
+			{
+				int tempKey = modeFilterList.removeLast();
+				int tempVal = modeMap.get(tempKey);
+				if (tempVal > 1)
+				{
+					modeMap.put(tempKey, tempVal -1);
+				}
+				else
+				{
+					modeMap.remove(tempKey);
+				}
+			}
+			else
+			{
+				modeFilterCnt++;
+			}
+			
+			// search map
+			Iterator<Integer> modeFilterIt = modeFilterList.iterator();
+			int tempKey = 0;
+			int tempVal = 0;
+			int tempMaxKey = 0;
+			int tempMaxVal = 0;
+			while (modeFilterIt.hasNext())
+			{
+				tempKey = modeFilterIt.next();
+				tempVal = modeMap.get(tempKey);
+				if (tempVal > tempMaxVal)
+				{
+					tempMaxKey = tempKey;
+					tempMaxVal = tempVal;
+				}
+			}
+			
+			
 			
 			
 			// compare to refPulse

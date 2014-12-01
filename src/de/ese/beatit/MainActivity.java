6f7731667df938b5341df8c6e46506e12af5d9a2
markus <markus@markus-desktop>
2014-12-01 10:57:03 +0100

    - service connection implemented - mp3 files from FileSystem are retrieved

--- src/de/ese/beatit/MainActivity.java
+++ src/de/ese/beatit/MainActivity.java
@@ -1,21 +1,77 @@
 package de.ese.beatit;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+import de.ese.beatit.beatanalyzer.BeatAnalyzerService;
 
 
 
 public class MainActivity extends Activity {
+    
+    /** the service which provides the track database **/
+    private BeatAnalyzerService beatAnalayzerService = null;
+	private boolean isBound = false;
+    
+	private ServiceConnection serviceConnection = new ServiceConnection() {
+	    public void onServiceConnected(ComponentName className, IBinder service) {
+	        // This is called when the connection with the service has been
+	        // established, giving us the service object we can use to
+	        // interact with the service.  Because we have bound to a explicit
+	        // service that we know is running in our own process, we can
+	        // cast its IBinder to a concrete class and directly access it.
+	    	beatAnalayzerService = ((BeatAnalyzerService.BeatAnalyzerServiceBinder)service).getService();
 
-    @Override
+	        // Tell the user about this for our demo.
+	        Toast.makeText(getApplicationContext(), "Connected",
+	                Toast.LENGTH_SHORT).show();
+	    }
+
+	    public void onServiceDisconnected(ComponentName className) {
+	        // This is called when the connection with the service has been
+	        // unexpectedly disconnected -- that is, its process crashed.
+	        // Because it is running in our same process, we should never
+	        // see this happen.
+	    	beatAnalayzerService = null;
+	        Toast.makeText(getApplicationContext(), "Disconnected",
+	        		Toast.LENGTH_SHORT).show();
+	    }
+	};
+	
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // connect service
+        doBindService();
     }
 
+	void doBindService() {
+	    // Establish a connection with the service.  We use an explicit
+	    // class name because we want a specific service implementation that
+	    // we know will be running in our own process (and thus won't be
+	    // supporting component replacement by other applications).
+	    bindService(new Intent(getApplicationContext(),
+	            BeatAnalyzerService.class), serviceConnection, Context.BIND_AUTO_CREATE);
+	    isBound = true;
+	}
+
+	void doUnbindService() {
+	    if (isBound) {
+	        // Detach our existing connection.
+	        unbindService(serviceConnection);
+	        isBound = false;
+	    }
+	}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,6 +90,12 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        doUnbindService();
     }
     
     /** Called when the user clicks the Send button */
--- src/de/ese/beatit/beatanalyzer/BeatAnalyzerService.java
+++ src/de/ese/beatit/beatanalyzer/BeatAnalyzerService.java
@@ -1,15 +1,25 @@
 package de.ese.beatit.beatanalyzer;
 
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Service;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
+import android.provider.MediaStore;
 import android.support.v4.util.Pair;
 import android.util.Log;
 
 public class BeatAnalyzerService extends Service {
 
-	/** Adapter class to communicate with service **/
+	private boolean DEBUG = true;
+	private String DEBUG_FS_PREFIX = "/storage/emulated/0/ese/";
+	
+	/** Adapter class to communicate with app **/
 	public class BeatAnalyzerServiceBinder extends Binder {
 		
 		private BeatAnalyzerService service;
@@ -26,6 +36,10 @@ public class BeatAnalyzerService extends Service {
 			this.service = service;
 		}
 	}
+	
+	private Timer timer = null;
+	private int timerIntervalMinutes = 2;
+	private int minDurationSeconds = 20;
 	
 	private BeatAnalyzerServiceBinder binder = new BeatAnalyzerServiceBinder(this);
 	
@@ -44,6 +58,29 @@ public class BeatAnalyzerService extends Service {
 		
 		// start task which periodically searches for MP3 Files
 		// which are not analyzed yet
+    	timer = new Timer();
+    	timer.scheduleAtFixedRate(new TimerTask() {
+
+    		@Override
+    		public void run() {
+    			analyzeFileSystem();
+    		}
+    		
+    	}, 0, timerIntervalMinutes * 1000 * 60);  
+	}
+	
+	public void analyzeFileSystem(){
+		
+		// get all mp3 files from system
+		ArrayList<String> mp3Files = mp3Files();
+		for(String s : mp3Files){
+			
+			// in debug only analyze files from given directory
+			if(DEBUG && !s.startsWith(DEBUG_FS_PREFIX)){
+				continue;
+			}
+			Log.e("beatit", s);
+		}
 	}
 	
 	@Override
@@ -65,4 +102,45 @@ public class BeatAnalyzerService extends Service {
 		return database.track(bpm);
 	}
 
+	ArrayList<String> mp3Files(){
+		
+		ArrayList<String> paths = new ArrayList<String>();
+		
+		String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
+		
+		String[] projection = {
+			MediaStore.Audio.Media.DATA,
+			MediaStore.Audio.Media.DURATION
+		};
+		final String sortOrder = MediaStore.Audio.AudioColumns.TITLE + " COLLATE LOCALIZED ASC";
+		
+		Cursor cursor = null;
+		try {
+			
+			Uri uri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+			cursor = getContentResolver().query(uri, projection, selection, null, sortOrder);
+			if( cursor != null){
+				cursor.moveToFirst();
+				while( !cursor.isAfterLast() ){
+					
+					String path = cursor.getString(0);
+					int songDuration = Integer.parseInt(cursor.getString(1));
+					
+					if(songDuration > minDurationSeconds && (path.endsWith(".mp3") || path.endsWith(".MP3"))){
+						paths.add(path);
+					}
+					cursor.moveToNext();
+				}
+			}
+			
+		} catch (Exception e) {
+			Log.e("beatit", e.toString());
+		} finally{
+			if( cursor != null){
+				cursor.close();
+			}
+		}
+		
+		return paths;
+	}
 }

8a5896f3822c72f77556b7d527c6a48b14b04de3
0xff01 <0xff01@gmail.com>
2015-01-14 18:42:54 +0100

    master branch for android studio

--- src/de/ese/beatit/mp3/BeatChangeListener.java
+++
@@ -1,6 +0,0 @@
-package de.ese.beatit.mp3;
-
-public interface BeatChangeListener {
-
-	public void onBPMChanged(int bpm);
-}
--- src/de/ese/beatit/mp3/MP3PlayerListener.java
+++
@@ -1,7 +0,0 @@
-package de.ese.beatit.mp3;
-
-public interface MP3PlayerListener {
-
-	public void onTrackChanged(Track track);
-	public void onPlaybackTimeChanged(double time);
-}

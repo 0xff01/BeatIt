41de441ecd71972e0972837cfb1574e01e7a26b9
debijan <debijan42@gmail.com>
2015-01-27 13:22:47 +0100

    DatabaseHelper needed to create SQLite database for storing music data DatabaseReader to provide interface to access database content

---
+++ app/src/main/java/de/ese/beatit/database/DatabaseHelper.java
@@ -0,0 +1,52 @@
+package de.ese.beatit.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by jan on 1/27/15.
+ */
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String TABLE_NAME = "musicDB";
+    public static final String COLUMN_FILE_PATH = "path";
+    public static final String COLUMN_TRACK_NAME = "track";
+    public static final String COLUMN_BPM = "bpm";
+    public static final String COLUMN_CERTAINTY = "certainty";
+    public static final String COLUMN_IS_CERTAIN = "isCertain";
+    public static final String COLUMN_DURATION = "duration";
+
+
+    private static final String DATABASE_NAME = "tracks.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    private static final String DATABASE_CREATE = "create table "
+            + TABLE_NAME + "("
+            + COLUMN_FILE_PATH + " text not null, "
+            + COLUMN_TRACK_NAME + " text not null, "
+            + COLUMN_BPM + " integer not null, "
+            + COLUMN_CERTAINTY + " REAL not null, "
+            + COLUMN_IS_CERTAIN + " integer not null, "
+            + COLUMN_DURATION + " REAL not null);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(DatabaseHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
---
+++ app/src/main/java/de/ese/beatit/database/DatabaseReader.java
@@ -0,0 +1,95 @@
+package de.ese.beatit.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.ese.beatit.beatanalyzer.BeatDescription;
+import de.ese.beatit.mp3.Track;
+
+/**
+ * Created by jan on 1/27/15.
+ */
+public class DatabaseReader {
+
+    private SQLiteDatabase db;
+    private DatabaseHelper dbHelper;
+    private String[] allColumns = {
+            DatabaseHelper.COLUMN_FILE_PATH,
+            DatabaseHelper.COLUMN_TRACK_NAME,
+            DatabaseHelper.COLUMN_BPM,
+            DatabaseHelper.COLUMN_CERTAINTY,
+            DatabaseHelper.COLUMN_IS_CERTAIN,
+            DatabaseHelper.COLUMN_DURATION
+    };
+
+    public DatabaseReader(Context context) {
+        dbHelper = new DatabaseHelper(context);
+    }
+
+    public void open() {
+        db = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public Track createTrackEntry(String dataPath, String track, double duration, BeatDescription description) {
+
+        ContentValues values = new ContentValues();
+        values.put(DatabaseHelper.COLUMN_FILE_PATH, dataPath);
+        values.put(DatabaseHelper.COLUMN_TRACK_NAME, track);
+        values.put(DatabaseHelper.COLUMN_BPM, description.getBpm());
+        values.put(DatabaseHelper.COLUMN_CERTAINTY, description.getCertainty());
+        values.put(DatabaseHelper.COLUMN_IS_CERTAIN, description.isCertain());
+        values.put(DatabaseHelper.COLUMN_DURATION, duration);
+
+        db.insert(DatabaseHelper.TABLE_NAME, null, values);
+
+        Cursor cursor = db.query(DatabaseHelper.TABLE_NAME, allColumns, null, null, null, null, null);
+        // Cursor cursor = db.rawQuery("select * from " +DatabaseHelper.TABLE_NAME, null);
+        cursor.moveToFirst();
+        Track newTrack = cursorToTrack(cursor);
+        cursor.close();
+        return newTrack;
+    };
+
+    private Track cursorToTrack(Cursor cursor) {
+
+        Track singleTrack = new Track();
+        singleTrack.setPath(cursor.getString(0));
+        singleTrack.setName(cursor.getString(1));
+        BeatDescription desc = new BeatDescription();
+        desc.setBpm(cursor.getInt(2));
+        desc.setCertainty(cursor.getDouble(3));
+        desc.isCertain();
+        singleTrack.setBeatDescription(desc);
+        singleTrack.setDuration(cursor.getDouble(4));
+
+        return singleTrack;
+    }
+
+    public List<Track> getAllTracks() {
+
+        List<Track> tracks = new ArrayList<Track>();
+
+        Cursor cursor = db.query(DatabaseHelper.TABLE_NAME, allColumns, null, null, null, null, null);
+
+        cursor.moveToFirst();
+
+        while(!cursor.isAfterLast()) {
+            Track track = cursorToTrack(cursor);
+            tracks.add(track);
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+
+        return tracks;
+    }
+}
